e9dba6826ff11b9a198c6b304635c72b30b8493a
MiguelMarquezRamirez <45156711+MiguelMarquezRamirez@users.noreply.github.com>
2019-04-15 23:37:36 -0500

    Add files via upload

---
+++ GestionClientes/src/Clientes.java
@@ -0,0 +1,65 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+
+public class Clientes extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Clientes frame = new Clientes();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Clientes() {
+		setTitle("Sistema de Gesti\u00F3n de Clientes");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 424, 21);
+		contentPane.add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Catalogos");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewMenu_1 = new JMenu("Clientes");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Asignaciones");
+		mnNewMenu_1.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Prospectos");
+		mnNewMenu_1.add(mntmNewMenuItem);
+		
+		JMenu mnNewMenu_2 = new JMenu("Herramientas");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenu mnNewMenu_3 = new JMenu("Salir");
+		menuBar.add(mnNewMenu_3);
+	}
+}
